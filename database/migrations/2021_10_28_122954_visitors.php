143ec6c6817d4320aa8a81db69b780d4821451fa
3° Batalhão de Suprimento - Dev <3bsup.dev@gmail.com>
2021-10-28 10:50:40 -0300

    up

---
+++ database/migrations/2021_10_28_122954_visitors.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Visitors extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->bigInteger('cpf');
+            $table->bigInteger('phone');
+            $table->tinyInteger('cnh');
+            $table->integer('enterprise');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('visitors');
+    }
+}
