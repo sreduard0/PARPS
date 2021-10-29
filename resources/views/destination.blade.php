33f466f7bbe4a7ea9a79147847b14433acfc4215
3° Batalhão de Suprimento - Dev <3bsup.dev@gmail.com>
2021-10-29 10:27:45 -0300

    up

--- resources/views/destination.blade.php
+++ resources/views/destination.blade.php
@@ -52,7 +52,6 @@
             </div>
             <!-- /.card-body -->
         </div>
-
     </section>
 @endsection
 @section('modal')
--- resources/views/reason.blade.php
+++ resources/views/reason.blade.php
@@ -23,22 +23,16 @@
                 <table id="table" class="table table-bordered table-striped">
                     <thead>
                         <tr>
-                            <th>Rendering engine</th>
-                            <th>Browser</th>
-                            <th>Platform(s)</th>
-                            <th>Engine version</th>
-                            <th>CSS grade</th>
+                            <th width="20">#</th>
+                            <th>Motivo</th>
+
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Trident</td>
-                            <td>Internet
-                                Explorer 4.0
-                            </td>
-                            <td>Win 95+</td>
-                            <td> 4</td>
-                            <td>X</td>
+                            <td>1</td>
+                            <td>Entrega de materiais</td>
+
                         </tr>
 
                     </tbody>
@@ -64,69 +58,10 @@
                 <div class="modal-body">
                     <div class="row">
                         <div class="form-group col">
-                            <label for="company_id">Visitante</label>
-                            <select class="select2" style="width: 100%;">
-                                <option selected="selected">Alabama</option>
-                                <option>Alaska</option>
-                                <option>California</option>
-                                <option>Delaware</option>
-                                <option>Tennessee</option>
-                                <option>Texas</option>
-                                <option>Washington</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="name">Fone:</label>
-                            <input type="text" class="form-control" id="name" name="name"
-                                placeholder="Digite seu nome completo" value="">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-md-3">
-                            <label>Entrada</label>
-                            <div class="input-group date" id="born_at" data-target-input="nearest">
-                                <input type="text" class="form-control" id="born_at" name="born_at" value="">
-                                <div class="input-group-text"><i class="fa fa-calendar"></i>
-                                </div>
-                            </div>
-                        </div>
+                            <label for="destination">Motivo</label>
+                            <input type="text" class="form-control" id="reason" name="reason"
+                                placeholder="Digite o motivo" value="">
 
-
-                        <div class="form-group col">
-                            <label for="company_id">Destino:</label>
-                            <select class="select2" style="width: 100%;">
-                                <option selected="selected">Alabama</option>
-                                <option>Alaska</option>
-                                <option>California</option>
-                                <option>Delaware</option>
-                                <option>Tennessee</option>
-                                <option>Texas</option>
-                                <option>Washington</option>
-                            </select>
-                        </div>
-                        <div class="form-group col">
-                            <label for="company_id">Motivo:</label>
-                            <select class="select2" style="width: 100%;">
-                                <option selected="selected">Alabama</option>
-                                <option>Alaska</option>
-                                <option>California</option>
-                                <option>Delaware</option>
-                                <option>Tennessee</option>
-                                <option>Texas</option>
-                                <option>Washington</option>
-                            </select>
-                        </div>
-                        <div class="form-group col">
-                            <label for="company_id">Crachá</label>
-                            <select class="select2" style="width: 100%;">
-                                <option selected="selected">Alabama</option>
-                                <option>Alaska</option>
-                                <option>California</option>
-                                <option>Delaware</option>
-                                <option>Tennessee</option>
-                                <option>Texas</option>
-                                <option>Washington</option>
-                            </select>
                         </div>
                     </div>
                 </div>
